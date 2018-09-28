48193df8e67a972ab87297b6f8b6e290f37e5b99
nick <nick_php@163.com>
2018-09-28 20:41:11 +0800

    :sparkles: 导航菜单配置

---
+++ .vuepress/config.js
@@ -0,0 +1,40 @@
+module.exports = {
+    title: 'vuepress',
+    description: 'vuepress-template description',
+    head: [
+        ['link', { rel: 'icon', href: '/images/favicon.png' }]
+    ],
+    themeConfig: {
+        nav: [
+            { text: '导航栏一', link: '/' },
+            { text: '导航栏二', link: '/docs/html/' },
+            { text: '导航栏三', link: '/docs/css/' },
+        ],
+        sidebar: [
+            {
+                title: '侧边栏一',
+                collapsable: false,
+                children: [
+                    '/docs/css/',
+                    '/docs/css/1.md'
+                ]
+            },
+            {
+                title: '侧边栏二',
+                collapsable: false,
+                children: [ 
+                    '/docs/html/',
+                    '/docs/html/1.md'
+                ]
+            },
+            {
+                title: '侧边栏三',
+                collapsable: false,
+                children: [ 
+                    '/docs/js/',
+                    '/docs/js/1.md'
+                ]
+            },
+        ]
+    }
+}
