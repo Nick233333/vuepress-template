4b2c244f4b7e37a4b34bd7f2d3c1fcbe579ed3ee
nick <nick_php@163.com>
2019-01-26 20:44:16 +0800

    :sparkles: add Valine

---
+++ docs/.vuepress/components/Valine.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="vcomments"></div>
+</template>
+
+<script>
+export default {
+    name: 'Valine',
+    mounted: function(){
+        // require window 
+        const Valine = require('valine');
+        if (typeof window !== 'undefined') {
+            this.window = window
+            window.AV = require('leancloud-storage')
+        
+        }
+     
+        new Valine({
+            el: '#vcomments' ,
+            appId: 'nKbBPn7copWpJhggo4BtA6XB-gzGzoHsz',// your appId
+            appKey: 'eWUrGuebD7jNVljOxMwwTtUI', // your appKey
+            notify: false, 
+            verify: false, 
+            avatar: 'mm', 
+            placeholder: '请输入评论' 
+        });
+    },
+}
+</script>
--- docs/css/README.md
+++ docs/css/README.md
@@ -1,1 +1,4 @@
 # css README.md
+
+
+<Valine></Valine>
